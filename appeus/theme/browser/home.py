d2361d34a9933cff0f2da513f7b2313a1287e0d6
Mikel Larreategi <mlarreategi@codesyntax.com>
2015-01-28 09:18:03 +0100

    new view based on summary_view for home-page

---
+++ appeus/theme/browser/home.py
@@ -0,0 +1,10 @@
+from plone.app.contenttypes.interfaces import ICollection
+from five import grok
+
+grok.templatedir('templates')
+
+
+class Home(grok.View):
+    grok.context(ICollection)
+    grok.require('zope2.View')
+    grok.name('home-view')
