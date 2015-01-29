15a4e0a20a7045a6681f2c691cc708aabf36ad12
Mikel Larreategi <mlarreategi@codesyntax.com>
2015-01-29 15:51:44 +0100

    use configured app

--- appeus/theme/browser/home.py
+++ appeus/theme/browser/home.py
@@ -1,5 +1,10 @@
-from plone.app.contenttypes.interfaces import ICollection
+from .interfaces import IAppEusFeaturedApp
 from five import grok
+from plone import api
+from plone.app.contenttypes.interfaces import ICollection
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+
 
 grok.templatedir('templates')
 
@@ -8,3 +13,15 @@ class Home(grok.View):
     grok.context(ICollection)
     grok.require('zope2.View')
     grok.name('home')
+
+    def get_selected_app(self):
+        registry = getUtility(IRegistry)
+        appeus_config = registry.forInterface(IAppEusFeaturedApp)
+        featured = appeus_config.featured_app
+        if featured:
+            catalog = api.portal.get_tool('portal_catalog')
+            content_items = catalog(UID=featured)
+            if content_items:
+                return content_items[0].getObject()
+
+        return None
